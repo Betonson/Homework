3999c78c815a920f64d80ec2cbc5d7a39a611f31
Betonson <leonel3289@mail.ru>
2020-08-03 18:38:23 +0300

    Добавлено решение 2-ой задачи

--- Задача 2.3/Задача 2.3/Program.cs
+++ Задача 2.3/Задача 2.3/Program.cs
@@ -8,8 +8,33 @@ namespace Задача_2._3
 {
     class Program
     {
+        static int GetInput()
+        {
+            int x;
+            string s;
+            bool flag;
+            do
+            {
+                Console.Write("Please enter a number:");
+                s = Console.ReadLine();
+                flag = int.TryParse(s, out x);
+                if (!flag) Console.Write("The recieved input is not a number. ");
+            } while (!flag);
+            return x;
+        }
         static void Main(string[] args)
         {
+            int x, sum=0;
+            do
+            {
+                x = GetInput();
+                if ((x % 2 != 0) && (x > 0))
+                {
+                    sum += x;
+                }
+            } while (x != 0);
+            Console.WriteLine(sum);
+            Console.ReadKey();
         }
     }
 }
