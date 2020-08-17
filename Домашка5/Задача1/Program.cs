7b04df80179b49e789db1bb7ac5f65dd04d19456
Betonson <leonel3289@mail.ru>
2020-08-17 18:20:40 +0300

    Задачи 1 и 2
    Добавлены решения 1 и 2 задач

--- Домашка5/Задача1/Program.cs
+++ Домашка5/Задача1/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace Задача1
 {
     class Program
     {
+        static bool CheckLoginSimple(string input)
+        {
+            if ((input.Length <2) || (input.Length > 10))
+            {
+                return false;
+            }
+            for (int i = 0;i<input.Length; i++)
+            {
+                if (!char.IsLetterOrDigit(input[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        //static bool CheckLoginRegex(string input)
+        //{
+
+        //}
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string inputStr;
+            Console.WriteLine("Введите логин:");
+            inputStr = Console.ReadLine();
+            Regex myReg = new Regex(@"\b[A-Za-z]{2,10}\b");
+            if (CheckLoginSimple(inputStr))
+            {
+                Console.WriteLine("Fine (Simple)");
+            }
+            else
+            {
+                Console.WriteLine("Not fine (Simple)");
+            }
+            Console.WriteLine(myReg.IsMatch(inputStr));
+            Console.ReadKey();
         }
     }
 }
---
+++ Домашка5/Задача2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+
+namespace Задача2
+{
+    class Program
+    {
+        static bool CheckPlease(string str1, string str2)
+        {
+            if (str1.Length != str2.Length) return false;
+            else
+            {
+                char[] masStr1 = str1.ToCharArray();
+                char[] masStr2 = str2.ToCharArray();
+                int pos;
+                for (int i = 0;i<str2.Length;i++)
+                {
+                    Console.WriteLine($"Searching for: {masStr2[i]}");
+                    pos = Array.IndexOf(masStr1, masStr2[i]);
+                    if (pos >= 0)
+                    {
+                        Array.Clear(masStr1, pos, 1);
+                        Console.WriteLine(masStr1);
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("..\\..\\data.txt");
+            string controlLine = sr.ReadLine();
+            string testLine = sr.ReadLine();
+            sr.Close();
+            if (CheckPlease(controlLine, testLine))
+            {
+                Console.WriteLine("Yep");
+            }
+            else
+            {
+                Console.WriteLine("Nope");
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
